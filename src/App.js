cdc7d9212119cb9cb96554e84d9133213135ca56
ritamroy18 <ritamroycs07@gmail.com>
2020-04-23 20:12:16 +0530

    ui changes

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import logo from './logo.svg';
-import './App.css';
+import './App.scss';
 import { Provider } from 'react-redux';
 import PostForm from './components/Postform';
 
--- src/components/Postform.js
+++ src/components/Postform.js
@@ -1,21 +1,25 @@
 import React, { Component } from 'react'
+import ReactDOM from 'react-dom';
+
 import { connect } from 'react-redux'
 import { newPosts } from '../actions/postActions'
-// import '../components/Postform.css';
-import '../App.css';
+import '../components/Postform.scss';
+import '../App.scss';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faSpinner } from '@fortawesome/free-solid-svg-icons'
 
 
+
 export class PostForm extends Component {
 
-    componentDidMount(){
+    componentDidMount() {
+
         const { posts, newPosts } = this.props;
         newPosts(posts.page);
     }
 
     showMore = () => {
-        const { posts, newPosts ,loading} = this.props;
+        const { posts, newPosts, loading } = this.props;
         // update the post page
         const pageNumber = posts.page;
         // newPosts(posts.page);
@@ -23,26 +27,28 @@ export class PostForm extends Component {
     }
 
     render() {
-      const { posts, loading } = this.props;
-      
+        const { posts, loading } = this.props;
+
         return (
             <div>
                 <h2>Image Gallery</h2>
-                <div className="flex-container" >
-                    {
-                        posts.items.map((post, i) =>
-                            <div className="content" key={i}>
-                                <img src = {post.urls['small']} />
-                            </div>
-                        )
-                     }
-                     </div>
- 
-                      {
-                        !loading.loading
-                        ? <FontAwesomeIcon icon={faSpinner} size="2x" onClick={this.showMore} />
-                        : <FontAwesomeIcon icon={faSpinner} size="2x" rotation={90} />
-                      } 
+                <div className="content">
+                    <section className="grid">
+                        {
+                            posts.items.map((post, i) =>
+                                <div data-key="{post.id}" className={post.height <= post.width ? 'item item-1' : 'item item-2'}>
+                                    <img src={post.urls['small']} />
+                                </div>
+                            )
+                        }
+                    </section>
+                </div>
+
+                {
+                    !loading.loading
+                        ? <FontAwesomeIcon icon={faSpinner} size="3x" onClick={this.showMore} />
+                        : <FontAwesomeIcon icon={faSpinner} size="3x" rotation={90} />
+                }
 
             </div>
         )
@@ -51,13 +57,13 @@ export class PostForm extends Component {
 const mapStateToProps = ({ posts, loading }) => ({
     posts,
     // loading: loading || posts.loading
-    loading 
+    loading
 
 });
 
 const mapDispatchToProps = dispatch => ({
-    newPosts: pageNumber => dispatch(newPosts({ posts: pageNumber, val: 10}))
-    
+    newPosts: pageNumber => dispatch(newPosts({ posts: pageNumber, val: 10 }))
+
 });
 
 export default connect(mapStateToProps, mapDispatchToProps)(PostForm);
